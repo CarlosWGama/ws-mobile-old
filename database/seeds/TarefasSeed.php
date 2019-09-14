b56b82529293a85460835ba18604bae7c5ee301e
Carlos W. Gama <carloswgama@gmail.com>
2019-09-14 16:07:51 -0300

    Ajustado para buscar o id do primeiro usuário

--- database/seeds/TarefasSeed.php
+++ database/seeds/TarefasSeed.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Tarefa;
+use App\Models\Usuario;
 
 class TarefasSeed extends Seeder
 {
@@ -11,17 +12,18 @@ class TarefasSeed extends Seeder
      * @return void
      */
     public function run() {
+        $usuario = Usuario::first();
         Tarefa::create([
             'descricao' => 'Teste',
             'data'      => date('Y-m-d'),
-            'usuario_id'=> 1,
+            'usuario_id'=> $usuario->id,
             'imagem'    => 'teste.png'
         ]);
 
         Tarefa::create([
             'descricao' => 'Teste 2',
             'data'      => date('Y-m-d', mktime(0, 0, 0, date('m')+2, date('d'), date('Y'))),
-            'usuario_id'=> 1,
+            'usuario_id'=> $usuario->id,
             'imagem'    => 'teste.png'
         ]);
     }
